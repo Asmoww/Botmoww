c325ea24a1d8ea793ae668aa9e63f7103edcaaf7
Asmoww <68480128+Asmoww@users.noreply.github.com>
2020-08-19 18:26:40 +0300

    Updated ban, added filter and filterlist

--- AdminCommands.cs
+++ AdminCommands.cs
@@ -17,40 +17,40 @@ namespace Botmoww.AdminCommands
         [RequireUserPermission(GuildPermission.BanMembers)]
         public async Task BanUserLengthReason(IGuildUser banUser, int banLenght, [Remainder] string banReason)
         {
-            //await Context.Guild.AddBanAsync(banUser, banLenght, banReason); }
-            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned for **{banLenght} days** :police_car:"); Globals.modCase += 1;
-            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **{banLenght} days** \n:envelope: Reason: **{banReason}**"); }
-            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance == 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
+            //await Context.Guild.AddBanAsync(banUser, banLenght, banReason); 
+            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned for **{banLenght} days**! :police_car:"); Globals.modCase += 1;
+            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **{banLenght} days** \n:envelope: Reason: **{banReason}** \n:bust_in_silhouette: ID: **{banUser.Id}**"); }
+            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance > 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
         }
         [Command("ban")]
         [RequireBotPermission(GuildPermission.BanMembers)]
         [RequireUserPermission(GuildPermission.BanMembers)]
         public async Task BanUserLength(IGuildUser banUser, int banLenght)
         {
-            //await Context.Guild.AddBanAsync(banUser, banLenght); }
-            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned for **{banLenght} days** :police_car:"); Globals.modCase += 1;
-            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **{banLenght} days** \n:envelope: Reason: **---**"); }
-            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance == 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
+            //await Context.Guild.AddBanAsync(banUser, banLenght); 
+            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned for **{banLenght} days**! :police_car:"); Globals.modCase += 1;
+            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **{banLenght} days** \n:envelope: Reason: **---** \n:bust_in_silhouette: ID: **{banUser.Id}**"); }
+            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance > 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
         }
         [Command("ban")]
         [RequireBotPermission(GuildPermission.BanMembers)]
         [RequireUserPermission(GuildPermission.BanMembers)]
         public async Task BanUser(IGuildUser banUser)
         {
-            //await Context.Guild.AddBanAsync(banUser); }
-            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned **permanently** :police_car:"); Globals.modCase += 1;
-            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **Permanent** \n:envelope: Reason: **---**"); }
-            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance == 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
+            //await Context.Guild.AddBanAsync(banUser); 
+            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned **permanently**! :police_car:"); Globals.modCase += 1;
+            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **Permanent** \n:envelope: Reason: **---** \n:bust_in_silhouette: ID: **{banUser.Id}**"); }
+            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance > 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
         }
         [Command("ban")]
         [RequireBotPermission(GuildPermission.BanMembers)]
         [RequireUserPermission(GuildPermission.BanMembers)]
         public async Task BanUserReason(IGuildUser banUser, [Remainder] string banReason)
         {
-            //await Context.Guild.AddBanAsync(banUser, banReason); }
-            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned **permanently** :police_car:"); Globals.modCase += 1;
-            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **Permanent** \n:envelope: Reason: **{banReason}**"); }
-            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance == 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
+            //await Context.Guild.AddBanAsync(banUser, banReason); 
+            await Context.Message.Channel.SendMessageAsync($">>> :hammer: **{banUser}** has been banned **permanently**! :police_car:"); Globals.modCase += 1;
+            try { await Globals.modLogChannel.SendMessageAsync($">>> **{Globals.modCase}** | {Globals.date} | {Globals.time} \n:hammer: **{Context.Message.Author}** banned **{banUser}** \n:clock4: Length: **Permanent** \n:envelope: Reason: **{banReason}** \n:bust_in_silhouette: ID: **{banUser.Id}**"); }
+            catch (NullReferenceException) { Globals.tipChance += 1; if (Globals.tipChance > 5) { Thread.Sleep(10); await Context.Message.Channel.SendMessageAsync($">>> **Tip**: You can set a moderation log channel by using **{Globals.botPrefix}setchannel** :blue_car:"); Globals.tipChance = 0; } }
         }
         #endregion
 
@@ -69,7 +69,7 @@ namespace Botmoww.AdminCommands
         #endregion
 
         #region kick
-
+        
         #endregion
 
         #region mute 
@@ -86,6 +86,28 @@ namespace Botmoww.AdminCommands
 
         #region pardon
 
+        #endregion
+
+        #region filter
+        [Command("filter")]
+        public async Task Filter(string filterWord)
+        {
+            if (filterWord.EndsWith('-') || filterWord.EndsWith('+'))
+            { if (filterWord.EndsWith('+'))
+                { if (Globals.filterList.Contains(filterWord.TrimEnd('+'))) { await Context.Message.Channel.SendMessageAsync($">>> The word **{filterWord.TrimEnd('+')}** already exists! :red_car:"); }
+                    else { Globals.filterList.Add(filterWord.TrimEnd('+')); await Context.Message.Channel.SendMessageAsync($">>> The word **{filterWord.TrimEnd('+')}** was added! :blue_car:"); } }                
+                else if (Globals.filterList.Contains(filterWord.TrimEnd('-'))) { Globals.filterList.Remove(filterWord.TrimEnd('-')); await Context.Message.Channel.SendMessageAsync($">>> The word **{filterWord.TrimEnd('-')}** was removed! :blue_car:"); }
+                else { await Context.Message.Channel.SendMessageAsync($">>> The word **{filterWord.TrimEnd('-')}** doesn't exist! :red_car:"); } }             
+            else await Context.Message.Channel.SendMessageAsync($">>> Incorrect character **{filterWord.Substring(filterWord.Length-1)}** :red_car: \nUse **+** to add and **-** to remove a word.");
+        }
+        #endregion
+
+        #region filterlist
+        [Command("filterlist")]
+        public async Task FilterList()
+        {
+            await Context.Channel.SendMessageAsync($">>> **Filter List** | **{Globals.filterList.Count}** \n{string.Join("\n", Globals.filterList)}");
+        }
         #endregion
     }
     public class InterAdminCommands : InteractiveBase
